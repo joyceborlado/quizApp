222fb2218ca0c3dc9ca3cc0abe272730e9fdfda2
joyceborlado <joycebondocoy@gmail.com>
2023-10-08 20:48:48 +0800

    Added js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// getting all required elements
+
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
