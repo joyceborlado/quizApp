01a7c64701136a60a51b05eb9f765b9cdab8702a
joyceborlado <joycebondocoy@gmail.com>
2023-10-08 21:08:08 +0800

    added js when buttons are clicked

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+
+// If start Quiz button clicked
+
+start_btn.onclick = () => {
+  info_box.classList.add("activeInfo"); //show the info box
+};
+
+// If Exit button clicked
+exit_btn.onclick = () => {
+  info_box.classList.remove("activeInfo"); //hide the info box
+};
+
+// If Continue button clicked
+continue_btn.onclick = () => {
+  info_box.classList.remove("activeInfo"); //hide the info box
+  quiz_box.classList.add("activeQuiz"); //show the quiz box
+};
