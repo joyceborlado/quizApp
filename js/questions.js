b5c77adb2fb7cfbef5d211c422bde41e4f9a6a73
joyceborlado <joycebondocoy@gmail.com>
2023-10-10 17:27:52 +0800

    added questions js

---
+++ js/questions.js
@@ -0,0 +1,15 @@
+// creatig an array and passing the number, questions, options and answers
+
+let questions = [
+    {
+        numb: 1,
+        question: "What is the most scientifically accepted theory about the origin of the universe?",
+        answer:
+        options: [
+            "Creationist theory",
+            "Big Bang theory",
+            "Steady State Theory",
+            "Oscillating Universe Theory",
+        ]
+    }
+]
