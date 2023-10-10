034a2863cf6252875edeeb792cefe31e51f00be3
joyceborlado <joycebondocoy@gmail.com>
2023-10-10 18:05:01 +0800

    getting questions and options from array

--- js/script.js
+++ js/script.js
@@ -21,4 +21,31 @@ exit_btn.onclick = () => {
 continue_btn.onclick = () => {
   info_box.classList.remove("activeInfo"); //hide the info box
   quiz_box.classList.add("activeQuiz"); //show the quiz box
+  showQuestions(2);
 };
+
+let que_count = 0;
+
+// getting questions and options from array
+
+function showQuestions(index) {
+  const que_text = document.querySelector(".que_text");
+  const option_list = document.querySelector(".option_list");
+  let que_tag = "<span>" + questions[index].question + "</span>";
+  let option_tag =
+    '<div class = "option">' +
+    questions[index].options[0] +
+    "<span></span></div>" +
+    '<div class = "option">' +
+    questions[index].options[1] +
+    "<span></span></div>" +
+    '<div class = "option">' +
+    questions[index].options[2] +
+    "<span></span></div>" +
+    '<div class = "option">' +
+    questions[index].options[3] +
+    "<span></span></div>";
+
+  que_text.innerHTML = que_tag;
+  option_list.innerHTML = option_tag;
+}
