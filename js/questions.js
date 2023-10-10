257c3bedb4ba84ca2ea8913fafc51d7f3d28a654
joyceborlado <joycebondocoy@gmail.com>
2023-10-10 17:37:31 +0800

    encoded the questions and answers

--- js/questions.js
+++ js/questions.js
@@ -1,15 +1,68 @@
 // creatig an array and passing the number, questions, options and answers
 
 let questions = [
-    {
-        numb: 1,
-        question: "What is the most scientifically accepted theory about the origin of the universe?",
-        answer:
-        options: [
-            "Creationist theory",
-            "Big Bang theory",
-            "Steady State Theory",
-            "Oscillating Universe Theory",
-        ]
-    }
-]
+  {
+    numb: 1,
+    question:
+      "What is the most scientifically accepted theory about the origin of the universe?",
+    answer: "Big Bang Theory",
+    options: [
+      "Creationist theory",
+      "Big Bang theory",
+      "Steady State Theory",
+      "Oscillating Universe Theory",
+    ],
+  },
+
+  {
+    numb: 2,
+    question:
+      "Which of the following theories suggests that the universe has no beginning or end?",
+    answer: "Steady State Theory",
+    options: [
+      "Creationist theory",
+      "Big Bang theory",
+      "Steady State Theory",
+      "Oscillating Universe Theory",
+    ],
+  },
+
+  {
+    numb: 3,
+    question:
+      "The key idea of this theory is that the universe goes through cycles of expansion and contraction.",
+    answer: "Oscillating Universe Theory",
+    options: [
+      "Creationist theory",
+      "Big Bang theory",
+      "Steady State Theory",
+      "Oscillating Universe Theory",
+    ],
+  },
+
+  {
+    numb: 4,
+    question:
+      "In this theory, the universe began as a singularity—an infinitely small, hot, and dense point",
+    answer: "Big Bang Theory",
+    options: [
+      "Creationist theory",
+      "Big Bang theory",
+      "Steady State Theory",
+      "Oscillating Universe Theory",
+    ],
+  },
+
+  {
+    numb: 5,
+    question:
+      "Which theory believes in a supreme being who created everything including humans?",
+    answer: "Creationist Theory",
+    options: [
+      "Creationist theory",
+      "Big Bang theory",
+      "Steady State Theory",
+      "Oscillating Universe Theory",
+    ],
+  },
+];
